42b56ddf48f9ec64ec55dfdb016eb2f34a511f16
EduardasV <33314027+EduardasV@users.noreply.github.com>
2018-12-18 16:20:32 +0000

    starting CLI

---
+++ _Project_03_CLI_GET_ASYNC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Project_03_CLI_GET_ASYNC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
